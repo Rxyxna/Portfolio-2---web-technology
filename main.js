f0fc619fa309c5c381cd005150e45aa7adedae67
Rxyxna <Rayanabaga@outlook.com>
2022-10-08 19:26:29 +0200

    music player button and text added

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 
-
+// Variables
 const dinoButton = document.querySelector("#Dinoimg")
 const dinoCount = document.querySelector("#total-dino-amount")
 
+//  Event listener
+    dinoButton.addEventListener("click", addOneToTotalDinoAmmount)
 
-dinoButton.addEventListener("click", addOneToTotalDinoAmmount)
 
+// function count
 function addOneToTotalDinoAmmount()
 {
 let dinoTotal = dinoCount.innerHTML
     dinoTotal++
     dinoCount.innerHTML = dinoTotal
 }
+
+// PLAY AND PAUSE AUDIO BUTTON  --> code from ( https://www.w3schools.com/jsref/met_audio_play.asp)
+let playButton = document.getElementById("funkAudio");
+
+function playAudio() {
+    playButton.play();
+}
+
+function stopAudio() {
+    playButton.pause();
+}
+
+
+
+
+
+
+
